ed5cdc7c5a9be9b13d5c2707de80de30fd3654a9
HarryLaboratory <soussana.h@hotmail.fr>
2025-01-29 21:37:55 +0200

    vite.config.js

--- Final-Project/Chef2You/frontend/vite.config.js
+++ Final-Project/Chef2You/frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +11,9 @@ export default defineConfig({
   },
   define: {
     'process.env': {
-      VITE_API_URL: '"https://final-project-0kbv.onrender.com/api"', 
+      VITE_API_URL: '"https://final-project-0kbv.onrender.com/api"', // Backend URL
     },
   },
-});
+})
 
 
